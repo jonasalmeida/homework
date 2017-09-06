5f4598f02e5e1232f03884d6f8f35e31b13b9b38
Jonas Almeida <jalmeida@mathbiol.org>
2017-09-06 10:11:42 -0400

    back to primitive document.getElementByID

--- prathu.js
+++ prathu.js
@@ -1,24 +1,7 @@
 
 function call(){
-   var xVal=parseInt(x.value);
-   var yVal=parseInt(y.value);
+   var xVal=parseInt(document.getElementById('x').value);
+   var yVal=parseInt(document.getElementById('y').value);
    document.getElementById("z").textContent=xVal+yVal
 }
 x.onkeyup=y.onkeyup=call
-
-
-
-/*
-   if(isNaN(x)||isNaN(y)){
-          alert("please enter a number");
-     }
-     else
-
-        {
-
-         var z=x+y;
-         alert("The sum is " +result);
-       }
-
-}
-*/
